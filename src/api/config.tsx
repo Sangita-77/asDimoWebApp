390baf254ca6c8be9b45335d521678fe34c2be86
developer <developer@dreamlogodesign.net>
2026-05-29 15:27:45 +0530

    update config

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "https://dreamgroupsindia.com/dev/asDimoBackend/api";
+// export const BASE_URL = "http://localhost:4000/api";
 export const basename = "/";
