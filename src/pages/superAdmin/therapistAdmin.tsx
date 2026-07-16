8ac7eb0c2d7720566000ba77486b4e37eb7400ba
developer <developer@dreamlogodesign.net>
2026-07-16 14:52:58 +0530

    therapist flag change

--- src/pages/superAdmin/therapistAdmin.tsx
+++ src/pages/superAdmin/therapistAdmin.tsx
@@ -12,7 +12,8 @@ const SupTherapist: React.FC = () => {
     <>
       <Heading1 text="Therapist Admin" />
       <GlobalTableList
-        flag={[3, 5]}
+        flag={[5]}
+        // 3
         columns={[
           { key: "name", title: "Name", sortable: true },
           { key: "admin_name", title: "Admin", sortable: true },
